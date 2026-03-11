1a27212a2abe35908eeca13f4af9c4160c56958b
nitishdola <nitish.dola@gmail.com>
2026-03-11 15:45:51 +0530

    URL Added

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api",
+  //baseURL: "http://localhost:8000/api",
+  baseURL : "http://128.199.20.79/recruitment_backend/public/api";
 });
